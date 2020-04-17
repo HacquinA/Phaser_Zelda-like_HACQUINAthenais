e41402a295651fa32010a378a165808488dc49ea
HacquinA <athenais.hacquin@gmail.com>
2020-04-17 17:43:20 +0200

    ajout house

--- zelda.js
+++ zelda.js
@@ -19,18 +19,30 @@ scene: {
 
 var game= new Phaser.Game(config);
 
-
+var cursors;
+var player;
+var game;
+var boutonFeu;
+var groupeBullets;
+var tir = 2;
+var house;
 
 function preload(){
+
 	this.load.image('background','assets/backM.png');
-	this.load.spritesheet('perso', 'assets/Vaisseau.png',{frameWidth: 27.6, frameHeight:20});
+	this.load.image('bullet','assets/Bullet.png');
+	this.load.spritesheet('perso', 'assets/Snay.png',{frameWidth: 28, frameHeight:26});
+	this.load.image('house','assets/house.png');
+
 	
 }
 
 function create() {
 
    this.add.image(512,385,'background');
+
    this.cursors = this.input.keyboard.createCursorKeys();
+   boutonFeu = this.input.keyboard.addKey('A');
 
 
    // Perso 
@@ -45,80 +57,108 @@ function create() {
 
 		this.anims.create({
 		    key: 'left',
-		    frames: this.anims.generateFrameNumbers('perso', { start: 0, end: 2 }),
+		    frames: this.anims.generateFrameNumbers('perso', { start: 2, end: 3 }),
 		    frameRate: 10,
 		    repeat: -1
-		});
-
-		this.anims.create({
-		    key: 'turn',
-		    frames: [ { key: 'perso', frame: 1 } ],
-		    frameRate: 20
 		});
 
 		this.anims.create({
 		    key: 'right',
-		    frames: this.anims.generateFrameNumbers('perso', { start: 0, end: 2 }),
+		    frames: this.anims.generateFrameNumbers('perso', { start: 0, end: 1 }),
 		    frameRate: 10,
 		    repeat: -1
 		});
 
 		this.anims.create({
+		    key: 'stop',
+		    frames: [ { key: 'perso', frame: 6 } ],
+		    frameRate: 20
+		});
+
+		this.anims.create({
 		    key: 'up',
-		    frames: this.anims.generateFrameNumbers('perso', { start: 0, end: 2 }),
+		    frames: this.anims.generateFrameNumbers('perso', { start: 0, end: 1 }),
 		    frameRate: 10,
 		    repeat: -1
 		});
 
 		this.anims.create({
 		    key: 'down',
-		    frames: this.anims.generateFrameNumbers('perso', { start: 0, end: 2 }),
+		    frames: this.anims.generateFrameNumbers('perso', { start: 2, end: 3 }),
 		    frameRate: 10,
 		    repeat: -1
 		});
 
-	
+		//tir Snay 
+
+		var direction = 'right';
+		this.groupeBullets = this.physics.add.group(); 
+
+		//House 
+
+		this.house = this.physics.add.group(); 
+		this.house = this.physics.add.sprite(120,100,'house');
+		this.house.setBounce(0.02);
+		this.house.setCollideWorldBounds(true);
+		this.physics.add.collider(this.house,this.player);
+
+
 
 }
 
 
 function update() {
 
- // Deplacement vaisseau 
+ // Deplacement perso
 
-		if (this.cursors.left.isDown)
-		{
-		   this.player.setVelocityX(-200);
+		if (this.cursors.left.isDown){
 
-		   this.player.anims.play('left', true);
+			this.player.setVelocityX(-200);
 
-		   this.player.direction = 'left';
+			this.player.setFlipX(true);
+
+			this.player.anims.play('left', true);
+
+			this.player.direction = 'left';
+
+
+
+		   
 		}
-		else if (this.cursors.right.isDown)
-		{
+		else if (this.cursors.right.isDown){
 		    this.player.setVelocityX(200);
 
-		    this.player.anims.play('right', true);
+		    this.player.setFlipX(false);
+
+		    this.player.anims.play('left', true);
 
 		    this.player.direction = 'right';
+
+
 		}
-		else
-		{
+		else{
 		    this.player.setVelocityX(0);
 
-		    this.player.anims.play('turn');
+		    this.player.anims.play('stop');
+
 		}
 
-		if (this.cursors.up.isDown)
-		{
-		   this.player.setVelocityY(-200);
+		if (this.cursors.up.isDown){
+               this.player.setVelocityY(-200);
 
-		   this.player.anims.play('up', true);
+               this.player.anims.play('up', true);
+        }
 
-		   this.player.direction = 'up';
-		}
-		if (this.cursors.down.isDown)
-		{
+        else if (this.cursors.down.isDown){
+                this.player.setVelocityY(400);
+                this.player.setFlipX(true);
+        }
+        else{
+                this.player.setVelocityY(0);
+
+        }
+
+		if (this.cursors.down.isDown){
 		   this.player.setVelocityY(200);
 
 		   this.player.anims.play('down', true);
@@ -126,7 +166,23 @@ function update() {
 		   this.player.direction = 'down';
 		}
 
-		
+	//tir Snay 
+
+	if ( Phaser.Input.Keyboard.JustDown(boutonFeu)) {
+	   if (this.player.direction == 'left') { 
+            this.coefDir = -1; 
+        } 
+       else{ 
+            this.coefDir = 1 
+        }
+                // on crée la balle a coté du joueur
+                var bullet = this.groupeBullets.create(this.player.x + (25 * this.coefDir), this.player.y - 4, 'bullet');
+                // parametres physiques de la balle.
+                bullet.body.allowGravity =false;
+                bullet.setVelocity(1000 * this.coefDir, 0);
+	}
+	
+	
 
 }
 
